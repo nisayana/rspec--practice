1058a96d1f0c10700b25eab7e5a5ff4b2bc89ce1
nisayana <nimaevasb@gmail.com>
2022-01-10 12:12:33 -0500

    completed tests

--- lib/median_of_two_sorted_arrays_with_different_lengths.rb
+++ lib/median_of_two_sorted_arrays_with_different_lengths.rb
@@ -1,3 +1,56 @@
 def median_of_two_sorted_arrays_with_different_lengths(arr1, arr2)
+    n = arr1.length
+    m = arr2.length
+
+    i = 0
+    j = 0
+
+    count = 0
+
+    m1 = -1
+    m2 = -1
+
+    if (n+m)%2 == 1
+        while count <= (n+m)/2
+            if (i != n && j != m)
+                if arr1[i] < arr2[j]
+                    m1 = arr1[i]
+                    i+=1
+                else
+                    m1 = arr2[j]
+                    j+=1
+                end
+            elsif i < n
+                m1 = arr1[i]
+                i+=1 
+            else
+                m1 = arr2[j]
+                j+=1
+            end
+            count+=1
+        end
+        return m1.to_f
+    else
+        while count <= (n+m)/2
+            m2 = m1
+            if (i != n && j != m)
+                if arr1[i] < arr2[j]
+                    m1 = arr1[i]
+                    i+=1
+                else
+                    m1 = arr2[j]
+                    j+=1
+                end
+            elsif i < n
+                m1 = arr1[i]
+                i+=1 
+            else
+                m1 = arr2[j]
+                j+=1
+            end
+            count+=1
+        end
+        return (m1.to_f + m2.to_f ) / 2
+    end
 
 end
--- spec/median_of_two_sorted_arrays_spec.rb
+++ spec/median_of_two_sorted_arrays_spec.rb
@@ -3,7 +3,7 @@ describe "#median_of_two_sorted_arrays_with_different_lengths" do
     it "finds the median of two sorted arrays with different lengths when the combined length of the two arrays is odd" do
         arr1 = [1,3]
         arr2 = [2]
-        expect(median_of_two_sorted_arrays_with_different_lengths(arr1, arr2)).to eq(2)
+        expect(median_of_two_sorted_arrays_with_different_lengths(arr1, arr2)).to be_within(0.01).of(2.0)
     end
 
     it "finds the median of two sorted arrays with different lengths when the combined length of the two arrays is even" do
@@ -15,13 +15,19 @@ describe "#median_of_two_sorted_arrays_with_different_lengths" do
     it "finds the median of two sorted arrays with different lengths when both arrays have only zeros" do
         arr1 = [0,0]
         arr2 = [0,0]
-        expect(median_of_two_sorted_arrays_with_different_lengths(arr1, arr2)).to be_within(0.01).of(2.5)
+        expect(median_of_two_sorted_arrays_with_different_lengths(arr1, arr2)).to be_within(0.01).of(0.0)
     end
 
     it "finds the median of two sorted arrays with different lengths when one array is empty and the other has one element" do
         arr1 = []
         arr2 = [1]
-        expect(median_of_two_sorted_arrays_with_different_lengths(arr1, arr2)).to be_within(0.01).of(2.5)
+        expect(median_of_two_sorted_arrays_with_different_lengths(arr1, arr2)).to be_within(0.01).of(1.0)
+    end
+
+    it "finds the median of two sorted arrays with different lengths when second array is empty and the other has one element" do
+        arr1 = [2]
+        arr2 = []
+        expect(median_of_two_sorted_arrays_with_different_lengths(arr1, arr2)).to be_within(0.01).of(2.0)
     end
 
 end
