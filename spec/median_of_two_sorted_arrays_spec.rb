173a5903b2c501f4185d1c165d914c9f481c91e3
nisayana <nimaevasb@gmail.com>
2022-01-09 19:59:58 -0500

    added one more rest. total 4

--- spec/median_of_two_sorted_arrays_spec.rb
+++ spec/median_of_two_sorted_arrays_spec.rb
@@ -12,9 +12,15 @@ describe "#median_of_two_sorted_arrays_with_different_lengths" do
         expect(median_of_two_sorted_arrays_with_different_lengths(arr1, arr2)).to be_within(0.01).of(2.5)
     end
 
-    it "finds the median of two sorted arrays with different lengths when the combined length of the two arrays is even" do
-        arr1 = [1,2]
-        arr2 = [3,4]
+    it "finds the median of two sorted arrays with different lengths when both arrays have only zeros" do
+        arr1 = [0,0]
+        arr2 = [0,0]
+        expect(median_of_two_sorted_arrays_with_different_lengths(arr1, arr2)).to be_within(0.01).of(2.5)
+    end
+
+    it "finds the median of two sorted arrays with different lengths when one array is empty and the other has one element" do
+        arr1 = []
+        arr2 = [1]
         expect(median_of_two_sorted_arrays_with_different_lengths(arr1, arr2)).to be_within(0.01).of(2.5)
     end
 
